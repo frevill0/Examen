4119899206f5a8b0da71896e000cb27db207ef2e
unknown <administrator@EST-30-08.quitotenis.local>
2024-11-04 22:43:40 -0500

    App mejorada

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,27 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'clase3',
-  webDir: 'www'
+  appName: 'Freddy',
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
